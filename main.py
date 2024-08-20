9f27a44a802a7ff8a8bec17b124122b8ac21a8c6
tnt1223 <104182198+tnt1223@users.noreply.github.com>
2024-08-20 18:21:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-import requests, json, re, os, time
-
+import requests
+import json
+import re
+import os
+import time
 session = requests.session()
 
-# url = 'https://ikuuu.me'
 url = os.environ.get('URL')
+SMTP= os.environ.get('SMTP')
+
 login_url = '{}/auth/login'.format(url)
 check_url = '{}/user/checkin'.format(url)
 logout_url = '{}/user/logout'.format(url)
-
-msg_template = 'http://www.pushplus.plus/send?token={}&title=ikuuu签到&content={}'
 
 header = {
         'origin': url,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
-def checkIn(email, passwd, SCKEY):
+def checkIn(email, passwd, SMTP):
     data = {
             'email': email,
             'passwd': passwd
@@ -28,25 +30,51 @@ def checkIn(email, passwd, SCKEY):
         print(result['msg'])
         content = result['msg']
         # 进行推送
-        if SCKEY != '':
-            push_url = msg_template.format(SCKEY, content)
-            requests.post(url=push_url)
+        if SMTP != '':
+            qqemail('ikuuu签到',content)
             print('推送成功')
         session.get(logout_url)
     except:
         content = '签到失败'
         print(content)
-        if SCKEY != '':
-            push_url = msg_template.format(SCKEY, content)
-            requests.post(url=push_url)
+        if SMTP != '':
+            qqemail('ikuuu签到',content)
         session.get(logout_url)
+
+def qqemail(subject,text):
+    #无需安装第三方库
+          #换成你的QQ邮箱SMTP的授权码(QQ邮箱设置里)  设置-> 账户-> SMTP服务
+    EMAIL_ADDRESS='821116234@qq.com'      #换成你的邮箱地址
+    EMAIL_PASSWORD=SMTP
+
+    import smtplib
+    smtp=smtplib.SMTP('smtp.qq.com',25)
+
+    import ssl
+    context=ssl.create_default_context()
+    sender=EMAIL_ADDRESS               #发件邮箱
+    receiver=EMAIL_ADDRESS#"255576170@qq.com"#EMAIL_ADDRESS
+                                          #收件邮箱
+    from email.message import EmailMessage
+    subject=subject
+    body=text
+    msg=EmailMessage()
+    msg['subject']=subject       #邮件主题
+    msg['From']=sender
+    msg['To']=receiver
+    msg.set_content(body)         #邮件内容
+
+    with smtplib.SMTP_SSL("smtp.qq.com",465,context=context) as smtp:
+        smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+        smtp.send_message(msg)
+
 
 if __name__ == '__main__':
     split = os.environ.get('INFO').split(',')
+
     for user in split:
         user_split = user.split('<split>')
         email = user_split[0]
         password = user_split[1]
-        sckey = user_split[2]
-        checkIn(email, password, sckey)
+        checkIn(email, password, SMTP)
         time.sleep(2)
