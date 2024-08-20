b4d8ce07bc7f5227512253a9c39ed0de39184257
tnt1223 <104182198+tnt1223@users.noreply.github.com>
2024-08-20 16:19:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logout_url = '{}/user/logout'.format(url)
 msg_template = 'http://www.pushplus.plus/send?token={}&title=ikuuu签到&content={}'
 
 header = {
-        'origin': 'https://ikuuu.eu',
+        'origin': url,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 def checkIn(email, passwd, SCKEY):
